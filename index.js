2aef04262f0d4e7de3f619abee4a3abce2a3a60a
inbal-fitness <97009435+inbal-fitness@users.noreply.github.com>
2022-01-03 06:10:50 +0200

    Version 1

---
+++ index.js
@@ -0,0 +1,12 @@
+function gotoWomen() {
+    location.href = "women.html";
+}
+function gotoSpecial() {
+    location.href = "special-needs.html";
+}
+function gotoParkinson() {
+    location.href = "parkinson.html";
+}
+function gotoHome() {
+    location.href = "index.html";
+}
